e520dde73fa48e882f1a969f3661190fb88dfb53
Emmanuel NIATI <emmanuel.niati@outlook.fr>
2022-10-25 19:01:49 +0200

    V100 : Initialisation du projet

--- src/fr/util/test/TestFO1.java
+++ src/fr/util/test/TestFO1.java
@@ -9,14 +9,17 @@ import java.util.StringTokenizer;
 public class TestFO1
 {
 	
-	public static void main( String[] args )
+	public static void traitement()
 	{
-		
+
 		BufferedReader bufferedReader;
 		FileReader fileReader;
+		
 		FileWriter fileWriter;
 		StringBuffer stringBuffer;
+		
 		String line;
+		
 		StringTokenizer stringTokenizer;
 
 		String pathIn = "C:\\Users\\emman\\eclipse-working\\in";
@@ -65,7 +68,79 @@ public class TestFO1
 			System.out.println( "IO Exception : " + ioe.getMessage() );
 			
 		}
+		
+	}
 
+	public static void main( String[] args )
+	{
+
+		BufferedReader bufferedReader;
+		FileReader fileReader;
+		
+		FileWriter fileWriter;
+		StringBuffer stringBuffer;
+		
+		String line;
+		
+		int i = 0;
+		
+		String pathIn = "C:\\Users\\emman\\eclipse-working\\in";
+		String pathOut = "C:\\Users\\emman\\eclipse-working\\out";
+		
+		try
+		{
+
+			fileReader = new FileReader( pathIn + "\\" + "blacklist.txt" );
+			
+			bufferedReader = new BufferedReader( fileReader );
+			stringBuffer = new StringBuffer();
+
+			while ( (line = bufferedReader.readLine()) != null)
+			{
+				
+				i++;
+				
+				String indice = "" + i;
+				
+				if( indice.length() == 1 )
+				{
+					indice = "000" + indice;
+				}
+				if( indice.length() == 2 )
+				{
+					indice = "00" + indice;
+				}
+				if( indice.length() == 3 )
+				{
+					indice = "0" + indice;
+				}
+				
+				// INSERT INTO `DISTRIBUTION`.`T_DIS_LISTENOIRE` (`ID`, `MAIL`) VALUES ('4408', 'dominique1.borie@intradef.gouv.fr');
+				
+				stringBuffer.append( "INSERT INTO `DISTRIBUTION`.`T_DIS_LISTENOIRE` (`ID`, `MAIL`) VALUES ('" );
+				stringBuffer.append( indice );
+				stringBuffer.append( "', " );
+				stringBuffer.append( line );
+				stringBuffer.append( ");" ).append("\r");
+				
+			}
+
+			stringBuffer.append( "\r" );
+			stringBuffer.append( "commit;" );
+			stringBuffer.append( "\r" );
+			
+			fileWriter = new FileWriter( pathOut + "\\" + "blacklist.txt" );
+			fileWriter.write( stringBuffer.toString() );
+			fileWriter.close();
+
+		} 
+		catch (IOException ioe)
+		{
+
+			System.out.println( "IO Exception : " + ioe.getMessage() );
+			
+		}
+		
 	}
 
 }
