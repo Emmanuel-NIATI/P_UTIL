9ceae943b40361f3c9799c3fb8f8d02f98499c56
Emmanuel-NIATI <emmanuel.niati@outlook.fr>
2026-03-25 10:08:50 +0100

    Update TestType.java

--- src/fr/util/test/TestType.java
+++ src/fr/util/test/TestType.java
@@ -2,8 +2,58 @@ package fr.util.test;
 
 public class TestType
 {
-  
-  public TestType()
+
+	/*
+
+	En Java, les types de données définissent la nature des valeurs qu’une variable peut stocker. Ils se divisent en deux grandes catégories : types primitifs et types de référence.
+
+
+	1) Types primitifs (8 au total). Ces types sont prédéfinis par Java et identifiés par des mots-clés :
+
+	byte :		entier signé 8 bits, plage de -128 à 127.
+	
+	short :		entier signé 16 bits, plage de -32 768 à 32 767.
+	
+	int :		entier signé 32 bits, plage de -2 147 483 648 à 2 147 483 647.
+	
+	long :		entier signé 64 bits, plage très large pour grands nombres.
+
+	float :		nombre à virgule flottante simple précision 32 bits (IEEE 754).
+
+	double :	nombre à virgule flottante double précision 64 bits.
+
+	boolean : 	valeurs logiques true ou false.
+
+	char : 		caractère Unicode 16 bits, plage de '\u0000' à '\uffff'.
+
+
+	2) Types de référence. Ils représentent des objets ou tableaux. Une variable de référence pointe vers une instance créée via un constructeur. La valeur par défaut est null.
+
+	Personne unPersonne = new Personne("Toto");
+
+
+	3) Littéraux en Java. C'est une valeur fixe écrite directement dans le code.
+
+	Numériques : décimal (int x = 42;), octal (int o = 052;), hexadécimal (int h = 0x2A;).
+
+	Caractères et chaînes : 'A', "Bonjour", avec séquences d’échappement (\n, \t, \\, etc.).
+
+	\n	Passe à la ligne
+	\r	Renvoie le curseur en début de ligne
+	\t	Tabulation
+	\v	Tabulation verticale
+	\b	Retour en arrière
+	\a	« alerte » (sonore ou visuelle)
+	
+	Points clés :
+	
+	Les types primitifs sont plus performants et consomment moins de mémoire.
+	
+	Les types de référence permettent de manipuler des structures complexes.
+
+	*/
+
+	public TestType()
 	{
 		
 	}
